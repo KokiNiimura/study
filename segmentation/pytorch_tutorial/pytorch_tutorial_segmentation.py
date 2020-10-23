fd83b6308ba9e9bb4c17228612103177f3061235
koki.niimura <koki.niimura@ghelia.com>
2020-10-23 19:04:19 +0900

    haven't finished coding PennFudanDatast class

---
+++ segmentation/pytorch_tutorial/pytorch_tutorial_segmentation.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+
+class PennFudanDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+
+        mask = Image.open(mask_path)
+        mask = np.array(mask)
+
+        obj_ids = np.unique(mask)
+        obj_ids = obj_ids[1:]
+
+        masks = mask == obj_ids[:, None, None]
+
+
+
+
